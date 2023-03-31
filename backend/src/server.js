1a83abb86164bf04b80c1da6f09f64c02d6283ce
Yohan Olmedo <yohancasaol@gmail.com>
2023-03-31 18:53:09 -0500

    Algunos cambios en el gitignore y el server para apuntar a la ruta "require('./database/db')"

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const morgan = require('morgan');
-require('./database/Db');
+require('./database/db');
 const server = express();
 
 const PORT = process.env.PORT || 3000
