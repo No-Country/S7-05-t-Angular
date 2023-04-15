c72c6c4e61031f446b04450b030a89fa4b906f26
agustin <agupalavecino97@gmail.com>
2023-04-15 14:59:35 -0300

    middleware para autenticacion de token

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,13 +4,17 @@ const teamLeadUserRouter = require("./teamLead.router");
 const studentUserRouter = require("./student.router");
 const authenticationRouter = require("./authentication.router");
 const teamRouter = require("./team");
+const authorization = require('../middlewares/autentificacion')
 
 const app = express();
 
 function routerApi(app) {
   app.use("/api/users/teamLead", teamLeadUserRouter);
+  // app.use("/api/users/teamLead", authorization.authorization, teamLeadUserRouter);
   app.use("/api/users/student", studentUserRouter);
+  // app.use("/api/users/student", authorization.authorization, studentUserRouter);
   app.use("/api/login", authenticationRouter);
   app.use("/api/team", teamRouter);
+  // app.use("/api/team", authorization.authorization, teamRouter);
 }
 module.exports = routerApi;
