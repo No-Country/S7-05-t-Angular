c8a98b2f946216679c1e182dbc613be0496ff2c7
agustin <agupalavecino97@gmail.com>
2023-04-19 16:57:06 -0300

    change api direction

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     name: 'dev',
-    url: 'http://localhost:3001/api/'
+    // url: 'http://localhost:3001/api/'
+    url: 'https://nocountrys705-production.up.railway.app/api/'
 };
   
